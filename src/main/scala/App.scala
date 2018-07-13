e5ee64bd8b21dbed1ffd562f66bcd7013d71ae72
Thy Ton <maithytonn@gmail.com>
2018-07-12 19:54:04 -0700

    Done with creating matrix

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -20,28 +20,22 @@ object App {
 
         printInfo(sequence)
 
-        renderPNG(sequence)
+        val matrix = createMatrix(sequence)
+        println(matrix)
 
     }
 
     val COLORS = Array(Aqua, Black, Blue, Fuchsia, Gray, Green, Lime, Maroon, Navy, Olive, Purple, Red, Silver, Teal, White, Yellow)
 
 
-    def renderPNG(sequence: Sequence) = {
+    def createMatrix(sequence: Sequence) = {
         val width = 128
-        var images: Seq[BufferedImage] = Vector()
+        val matrix = new HashMap[(Int, Int, Int, Int), Option[Int]]().withDefaultValue(None)
 
         (sequence.getTracks zipWithIndex) foreach { case (track, trackNumber) =>
             val height = track.ticks.toInt
 
-            val activeNotes = new HashMap[(Int, Int), Long]()
-
-            val bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
-            images = images :+ bi
-            val g = bi.createGraphics()
-
-            g.setBackground(new Color(0, 0, 0, 0))
-            g.clearRect(0, 0, width, height)
+            val activeNotes = new HashMap[(Int, Int), (Long, Int)]()
 
             (0 until track.size) map {track get _} foreach { event =>
                 val tick = event.getTick
@@ -50,35 +44,29 @@ object App {
                         val channel = sm.getChannel
                         val key = sm.getData1
                         val velocity = sm.getData2
-                        activeNotes += (((channel, key), tick))
+
+                        activeNotes += (((channel, key), (tick, velocity)))
                     }
                     case (sm:ShortMessage) if (sm.getCommand == ShortMessage.NOTE_OFF) => {
                         val channel = sm.getChannel
                         val key = sm.getData1
-                        val velocity = sm.getData2
-                        val startTick = activeNotes((channel, key))
+                        val endVelocity = sm.getData2
+                        val (startTick, velocity) = activeNotes((channel, key))
+                        val endTick = tick
                         activeNotes -= ((channel, key))
 
-                        g.setColor(COLORS(channel))
-                        g.drawLine(key, startTick.toInt, key, tick.toInt)
+                       
+                        (startTick until endTick) foreach {tick =>
+                          matrix += ((trackNumber, channel, key, tick.toInt) -> Some(velocity))
+                        }
+                        matrix += ((trackNumber, channel, key, endTick.toInt) -> Some(endVelocity))
+
                     }
                     case _ => {}
                 }
             }
         }
-
-        val maxHeight = images map {_ getHeight} max
-        val bi = new BufferedImage(width, maxHeight, BufferedImage.TYPE_INT_ARGB)
-        val g = bi.createGraphics()
-        val transform = new AffineTransform(1f,0f,0f,1f,0,0)
-        g.setBackground(Color.WHITE)
-        g.clearRect(0, 0, width, maxHeight)
-        images foreach { g.drawImage(_, transform, null) }
-
-        val tx = AffineTransform.getScaleInstance(1, -1);
-        tx.translate(0, -bi.getHeight(null));
-        val op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-        ImageIO.write(op.filter(bi, null), "PNG", new File("output.png"))
+        matrix
     }
 
     def printInfo(sequence: Sequence) =
